1a995e31fa35f2881c4ff310ee3460e2841a4f78
Juan Guillermo Murillo Castillo <jg.murillo10@uniandes.edu.co>
2017-02-12 23:07:16 -0500

    skeleton

---
+++ src/app.js
@@ -0,0 +1,38 @@
+(function () {
+	/* IIFE*/
+
+	'use strict';
+
+
+	angular.module('JuanMurilloApp', ['ui.router', 'ui.bootstrap'])
+	.controller('MainController', MainController)
+	.controller('TimelineController', TimelineController);
+
+
+	function MainController () {
+		// body... 
+		var ctrl = this;
+
+	};
+	function TimelineController () {
+		// body... 
+	};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*IIFE*/
+})();
